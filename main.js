ed4230b154a3871c3c88ddf2ff42c0a796fbdd08
Ranveer0909 <84077077+Ranveer0909@users.noreply.github.com>
2023-03-15 22:52:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 song1 = "";
 song2 = "";
 
-function setup()
-{
-    canvas = createCanvas(600,500);
-    canvas.center();
-
-    video = createCapture(VIDEO);
-    video.hide();
-}
-
 function preload()
 {
    song1 = loadSound("music.mp3");
@@ -20,3 +11,39 @@ function draw()
 {
     image(video, 0, 0, 600, 500);
 }
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+    console.log("Posenet has been loaded");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
+
